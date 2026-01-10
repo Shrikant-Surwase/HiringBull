29b1cc13ceea737b1f669aefdad412c6890d9b4e
Nayaker <nayak.primary@gmail.com>
2026-01-10 18:46:46 +0530

    sync

--- webclient/src/firebase.js
+++ webclient/src/firebase.js
@@ -7,13 +7,12 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAQbXgvW7gaLVjRj3fzBgrygO_BwNGfiwY",
-  authDomain: "scale8ai.firebaseapp.com",
-  projectId: "scale8ai",
-  storageBucket: "scale8ai.firebasestorage.app",
-  messagingSenderId: "197002889333",
-  appId: "1:197002889333:web:cb5b5b3bc4211c9c75103c",
-  measurementId: "G-ZS5VPHCDWY"
+  apiKey: "AIzaSyCdbui66J5d9lulSr8to75lS0bLCZTqpZA",
+  authDomain: "hiringbull-in.firebaseapp.com",
+  projectId: "hiringbull-in",
+  storageBucket: "hiringbull-in.firebasestorage.app",
+  messagingSenderId: "986237537878",
+  appId: "1:986237537878:web:607825cbc13817d78675f7"
 };
 
 // Initialize Firebase
